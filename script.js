efd0aecf54b82b4668f38bf6aefd95d2b4b2a5df
ChanEilFhios <bobrien@gmail.com>
2019-07-23 17:09:28 -0400

    only display coords from position on page

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const speedSpan = document.getElementById("speed")
 
 if (navigator && navigator.geolocation) {
   navigator.geolocation.getCurrentPosition((position) => {
-    speedSpan.innerHTML = JSON.stringify(position, null, 2)
+    speedSpan.innerHTML = JSON.stringify(position.coords, null, 2)
   })
 } else {
   console.log("This page requires geolocation services to function!")
