53a0c7ede38090fd3e35b16d54fd8f2603fd55f7
ChanEilFhios <bobrien@gmail.com>
2019-07-23 17:34:00 -0400

    Create serializer to allow coords to be displayed

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const speedSpan = document.getElementById("speed")
 
+const serializeCoords = coords => {
+  const props = []
+  for (let n in coords) {
+      props.push(`${n} = ${coords[n]}`)
+  }
+	return props.join('\n')
+}
+
 if (navigator && navigator.geolocation) {
   navigator.geolocation.getCurrentPosition((position) => {
-    speedSpan.innerHTML = JSON.stringify(position.coords, null, 2)
+    speedSpan.innerHTML = serializeCoords(position.coords)
   })
 } else {
   console.log("This page requires geolocation services to function!")
