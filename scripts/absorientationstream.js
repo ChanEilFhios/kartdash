ab623fadc9c5138e20ca7f6f46f5816a3439df6e
Brendan O'Brien <brendan.obrien@kroger.com>
2019-08-09 11:17:15 -0400

    Fix naming scheme

--- scripts/absorientationstream.js
+++ scripts/absorientationstream.js
@@ -13,5 +13,5 @@ export function createNewAbsOrientationStream(options) {
   .map(e => e.target)
 }
 
-export const calcHeadingFromQuaternion = ({q}) => Math.round(Math.atan2(2 * q[0] * q[1] + 2 * q[2] * q[3], 1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]) * (180 / Math.PI))
+export const calcHeadingFromQuaternion = ({quarternion: q}) => Math.round(Math.atan2(2 * q[0] * q[1] + 2 * q[2] * q[3], 1 - 2 * q[1] * q[1] - 2 * q[2] * q[2]) * (180 / Math.PI))
 export const normalizeHeading = heading => (heading < 0) ? heading += 360 : heading
