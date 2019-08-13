0c100e2f84eb3b8527c56ed7ddaf1390975cb217
Brendan O'Brien <brendan.obrien@kroger.com>
2019-08-13 14:49:18 -0400

    Fix typo

--- scripts/domutils.js
+++ scripts/domutils.js
@@ -6,5 +6,5 @@ export const elUpdater = id => {
 
 export const setElStyle = (el, attr, value) => el.style[attr] = value
 export const showElIfTrue = el => value => setElStyle(el, "display", value ? "block" : "none")
-export const hideElIfFalse = el => value => setElStyle(el, "display", !value ? "block" : "none")
+export const showElIfFalse = el => value => setElStyle(el, "display", !value ? "block" : "none")
 export const displayEl = (el, show) => setElStyle(el, "display", show ? "block" : "none")
